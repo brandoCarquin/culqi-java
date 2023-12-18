77c97203f55a6867d6350031d373e7c69e4dab1f
brandoCarquin <bando.carquin@culqi.com>
2023-12-17 22:46:21 -0500

    feat(*) fix endpoints

--- src/main/java/com/culqi/apioperation/service/Plan.java
+++ src/main/java/com/culqi/apioperation/service/Plan.java
@@ -1,11 +1,18 @@
 package com.culqi.apioperation.service;
-
+import com.culqi.model.ResponseCulqi;
+import com.culqi.apioperation.ObjectResult;
+import java.util.Map;
 
 public class Plan extends Generic {
 
-    private static final String URL = "/plans/";
+    private static final String URL = "/recurrent/plans";
+    private static final String CREATE_URL = URL + "/create";  // Corrección aquí
     
     public Plan() {
-    	super(URL);
+        super(URL);
+    }
+
+    public ResponseCulqi createPlan(Map<String, Object> body) throws Exception {
+        return new ObjectResult().create(body, CREATE_URL);
     }
 }
--- src/main/java/com/culqi/apioperation/service/Subscription.java
+++ src/main/java/com/culqi/apioperation/service/Subscription.java
@@ -1,11 +1,19 @@
 package com.culqi.apioperation.service;
+import com.culqi.model.ResponseCulqi;
+import com.culqi.apioperation.ObjectResult;
+import java.util.Map;
+
 
 public class Subscription extends Generic {
 
-    private static final String URL = "/subscriptions/";
-    
+    private static final String URL = "/recurrent/subscriptions";
+    private static final String CREATE_URL = URL + "/create";
     public Subscription() {
     	super(URL);
+    }
+    
+    public ResponseCulqi createSubscription(Map<String, Object> body) throws Exception {
+        return new ObjectResult().create(body, CREATE_URL);
     }
 
 }
